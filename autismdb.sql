79d32d442c7cce84eb4561640efbb5244e6aac85
annguyen0505 <ancm.edu@gmail.com>
2017-10-11 22:09:37 +0700

    update database

--- autismdb.sql
+++ autismdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2017 at 10:01 AM
+-- Generation Time: Oct 11, 2017 at 05:08 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -45,9 +45,9 @@ CREATE TABLE `child` (
   `date_of_birth` datetime DEFAULT NULL,
   `father_name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `mother_name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-  `deleted` tinyint(1) DEFAULT NULL,
   `extra_info_id` int(11) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL
+  `date_created` datetime DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -69,6 +69,18 @@ CREATE TABLE `child_child` (
 --
 
 CREATE TABLE `child_solution` (
+  `c_id` int(11) DEFAULT NULL,
+  `s_id` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `child_solution_recommend`
+--
+
+CREATE TABLE `child_solution_recommend` (
   `c_id` int(11) DEFAULT NULL,
   `s_id` int(11) DEFAULT NULL,
   `rating` int(11) DEFAULT NULL
@@ -135,6 +147,17 @@ CREATE TABLE `solution` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `solution_like`
+--
+
+CREATE TABLE `solution_like` (
+  `s_id` int(11) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `test`
 --
 
@@ -195,6 +218,15 @@ CREATE TABLE `token` (
   `token` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `token`
+--
+
+INSERT INTO `token` (`tk_id`, `u_id`, `token`) VALUES
+(1, 2, 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35'),
+(2, 2, '62f67d43ced797b39b090dcd8ff61541cefeb3a5b841116a87ee7a6cd5326eaf'),
+(3, 2, '566dfba2886807e0767882cdde813e312b0ddb525f76084d11db94e94f48c5e2');
+
 -- --------------------------------------------------------
 
 --
@@ -210,8 +242,17 @@ CREATE TABLE `user` (
   `phone` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `email` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `age` datetime DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL
+  `date_created` datetime DEFAULT NULL,
+  `role` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_id`, `userName`, `password`, `fullName`, `address`, `phone`, `email`, `age`, `date_created`, `role`) VALUES
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Nguy?n Admin', 'QN', '0976679753', 'adcm.edu@gmail', '1994-01-05 00:00:00', '2017-10-08 00:00:00', 0),
+(2, 'user01', 'aad415a73c4cef1ef94a5c00b2642b571a3e5494536328ad960db61889bd9368', 'Nguyễn Khắc Ẩn', NULL, NULL, NULL, NULL, NULL, 2);
 
 --
 -- Indexes for dumped tables
@@ -245,6 +286,13 @@ ALTER TABLE `child_solution`
   ADD KEY `child_solution_fk1` (`s_id`);
 
 --
+-- Indexes for table `child_solution_recommend`
+--
+ALTER TABLE `child_solution_recommend`
+  ADD KEY `c_id` (`c_id`),
+  ADD KEY `s_id` (`s_id`);
+
+--
 -- Indexes for table `examination`
 --
 ALTER TABLE `examination`
@@ -273,6 +321,13 @@ ALTER TABLE `income_type`
 ALTER TABLE `solution`
   ADD PRIMARY KEY (`s_id`),
   ADD KEY `solution_fk0` (`u_id`);
+
+--
+-- Indexes for table `solution_like`
+--
+ALTER TABLE `solution_like`
+  ADD KEY `s_id` (`s_id`),
+  ADD KEY `u_id` (`u_id`);
 
 --
 -- Indexes for table `test`
@@ -305,7 +360,8 @@ ALTER TABLE `test_type`
 -- Indexes for table `token`
 --
 ALTER TABLE `token`
-  ADD PRIMARY KEY (`tk_id`);
+  ADD PRIMARY KEY (`tk_id`),
+  ADD KEY `u_id` (`u_id`);
 
 --
 -- Indexes for table `user`
@@ -366,12 +422,12 @@ ALTER TABLE `test_type`
 -- AUTO_INCREMENT for table `token`
 --
 ALTER TABLE `token`
-  MODIFY `tk_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -397,6 +453,13 @@ ALTER TABLE `child_solution`
   ADD CONSTRAINT `child_solution_fk1` FOREIGN KEY (`s_id`) REFERENCES `solution` (`s_id`);
 
 --
+-- Constraints for table `child_solution_recommend`
+--
+ALTER TABLE `child_solution_recommend`
+  ADD CONSTRAINT `child_solution_recommend_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `child` (`c_id`),
+  ADD CONSTRAINT `child_solution_recommend_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `solution` (`s_id`);
+
+--
 -- Constraints for table `examination`
 --
 ALTER TABLE `examination`
@@ -419,6 +482,13 @@ ALTER TABLE `solution`
   ADD CONSTRAINT `solution_fk0` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
 
 --
+-- Constraints for table `solution_like`
+--
+ALTER TABLE `solution_like`
+  ADD CONSTRAINT `solution_like_ibfk_1` FOREIGN KEY (`s_id`) REFERENCES `solution` (`s_id`),
+  ADD CONSTRAINT `solution_like_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+
+--
 -- Constraints for table `test`
 --
 ALTER TABLE `test`
@@ -430,6 +500,12 @@ ALTER TABLE `test`
 ALTER TABLE `test_rule_detail`
   ADD CONSTRAINT `test_rule_detail_fk0` FOREIGN KEY (`test_rule_id`) REFERENCES `test_rule` (`test_rule_id`),
   ADD CONSTRAINT `test_rule_detail_fk1` FOREIGN KEY (`type_id`) REFERENCES `test_type` (`type_id`);
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `token_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
